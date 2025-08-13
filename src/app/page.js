383b245046c990261f97cc5488cd590a75b44903
Yashraj Sharma <yashrajsharma@Yashrajs-MacBook-Air.local>
2025-08-13 14:00:38 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,11 @@
 // app/page.js
-'use client'; // make this a client component
+'use client'; 
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+
+import AuthPage from './auth/page';
 
 export default function Home() {
-  const router = useRouter();
 
-  useEffect(() => {
-    router.replace('/auth'); // client-side redirect
-  }, [router]);
+ return <AuthPage/>
 
-  return null; // nothing renders on root
 }
