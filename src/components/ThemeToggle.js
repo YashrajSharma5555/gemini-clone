10dd67495f8ca85670e111dfa9cfc0c91a04f43a
Yashraj Sharma <yashrajsharma@Yashrajs-MacBook-Air.local>
2025-08-13 15:14:17 +0530

    first commit

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
-import {Button} from './ui/Button'
+import {Button} from '@/components/ui/Button'
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
